68bbe573160e73bfe18b3c4c5bd345884346a8d3
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2021-01-21 15:11:49 -0300

    init

---
+++ carteira/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import CompraModel, VendaModel
+
+class CompraSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = CompraModel
+        fields = ['__all__']
+
+class VendaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = CompraModel
+        fields = ['__all__']
